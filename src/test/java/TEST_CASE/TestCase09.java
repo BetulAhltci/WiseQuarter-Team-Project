53711ceee4ebcf551966a92d1681c111e6158143
BetulAhlatci <betulahlatci13@gmail.com>
2023-03-08 20:46:34 +0300

    betul 08.03 v1

---
+++ src/test/java/TEST_CASE/TestCase09.java
@@ -0,0 +1,36 @@
+package TEST_CASE;
+
+public class TestCase09 {
+    /*
+    1) Open the browser
+        tarayıcıyı aç
+    2) Enter the URL “http://practice.automationtesting.in/”
+        "http://practice.automationtesting.in/" URL'sini girin
+    3) Click on Shop Menu
+        Mağaza Menüsüne tıklayın
+    4) Now click on Home menu button
+        Şimdi Ana menü düğmesine tıklayın
+    5) Test whether the Home page has Three Arrivals only
+        Ana sayfada yalnızca Üç Varış olup olmadığını test edinn
+    6) The Home page must contains only three Arrivals
+        Ana sayfa yalnızca üç Varış içermelidir
+    7) Now click the image in the Arrivals
+        Şimdi Gelenler'deki resme tıklayın
+    8) Test whether it is navigating to next page where the user can add that book into his basket.
+        Kullanıcının o kitabı sepetine ekleyebileceği bir sonraki sayfaya gidip gitmediğini test edin.
+    9) Image should be clickable and shoul navigate to next page where user can add that book to his basket
+        Resim tıklanabilir olmalı ve kullanıcının o kitabı sepetine ekleyebileceği bir sonraki sayfaya gitmelidir.
+    10) Click on the Add To Basket button which adds that book to your basket
+        O kitabı sepetinize ekleyen Sepete Ekle düğmesine tıklayın
+    11) User can view that Book in the Menu item with price.
+        Kullanıcı o Kitabı Menü öğesinde fiyatla görüntüleyebilir.
+    12) Now click on Item link which navigates to proceed to check out page.
+        Şimdi, ödeme sayfasına ilerlemek için gezinen Öğe bağlantısına tıklayın.
+    13) User can click on the Item link in menu item after adding the book in to the basket which leads to the check out page
+        Kullanıcı, kitabı sepete ekledikten sonra, ödeme sayfasına yönlendiren menü öğesindeki Öğe bağlantısına tıklayabilir.
+    14) Enter the Coupon code as ‘krishnasakinala’ to get 50rps off on the total.
+         Toplamda 50rps indirim kazanmak için Kupon kodunu "krishnasakinala" olarak girin.
+    15) User can able to apply coupon by entering ‘krishnasakinala’ in the coupon textbox which give 50rps off on the total price
+        Kullanıcı, toplam fiyat üzerinden 50rps indirim sağlayan kupon metin kutusuna "krishnasakinala" yazarak kupon uygulayabilir
+     */
+}
